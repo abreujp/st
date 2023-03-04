8c813a08400b790616b8db8d58227ea4f97a66e2
jp7webdesign <jpaulo6ba@gmail.com>
2023-03-04 16:38:59 -0300

    change font

--- config.h
+++ config.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetBrainsMono Nerd Font:bold:pixelsize=16:antialias=true:autohint=true";
-static char *font2[] = { "NotoColorEmoji:pixelsize=10:antialias=true:autohint=true" };
+static char *font = "JetBrainsMono Nerd Font:bold:pixelsize=18:antialias=true:autohint=true";
+static char *font2[] = { "NotoColorEmoji:pixelsize=16:antialias=true:autohint=true" };
 static int borderpx = 2;
 
 /*
