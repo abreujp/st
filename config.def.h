3296764fe76a2f4d37beb1c660b08dd070d5d2ce
jp7webdesign <jpaulo6ba@gmail.com>
2023-03-22 10:46:59 -0300

    change files

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetBrainsMono Nerd Font:bold:size=12";
+static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
