718daf075a08d17bc90c0dd2b563ed6fd0a43076
jpstudioweb <jpaulo6ba@gmail.com>
2024-05-25 09:39:12 -0300

    update

--- config.h
+++ config.h
@@ -8,7 +8,7 @@
 /* static char *font = "JetBrainsMono Nerd Font Mono:style=bold:size=10:antialias=true:autohint=true"; */
 
 static const char *fonts[] = {
-    "CaskaydiaMono Nerd Font Mono:style=Bold:size=12:antialias=true:autohint=true",
+    "JetBrainsMono Nerd Font:style=Bold:size=12:antialias=true:autohint=true",
     "Noto Color Emoji:pixelsize=12:antialias=true:autohint=true"
 };
 static int borderpx = 2;
